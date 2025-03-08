9059280a6eaae0818c13c86131bc51a8c67711c8
deviana97 <mateusviana97@gmail.com>
2025-03-08 15:16:43 -0300

    add routes config

---
+++ src/config/routes.ts
@@ -0,0 +1,22 @@
+export const Routes = {
+  changePassword: '/change-password',
+  home: '/',
+  login: '/login',
+  logout: '/logout',
+  report: '/report',
+  settings: '/settings/',
+  signup: '/signup',
+  profile: '/profile',
+};
+
+function buildUrlParam(baseUrl: string, params: { [key: string]: string | number | boolean | Array<string | number | boolean> }): string {
+  const queryString = Object.entries(params)
+    .flatMap(([key, value]) =>
+      Array.isArray(value)
+        ? value.map(v => `${encodeURIComponent(key)}=${encodeURIComponent(v)}`)  // Trata arrays
+        : `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    )
+    .join('&');
+
+  return queryString ? `${baseUrl}?${queryString}` : baseUrl;
+}
