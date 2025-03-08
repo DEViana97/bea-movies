4f1d56feac85d5b10f298dc03d60467eb7b31883
deviana97 <mateusviana97@gmail.com>
2025-03-08 15:17:14 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
-import type { NextConfig } from "next";
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   compiler: {
+//     styledComponents: true,
+//   }
+// };
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// export default nextConfig;
+
+const nextConfig = {  
+  reactStrictMode: false, // Ativa o Strict Mode para ajudar na detecção de problemas
+  compiler: {
+    styledComponents: true, // Ativa suporte ao styled-components
+  },
+  // images: {
+  //   domains: ['example.com', 'cdn.example.com'], // Permite carregar imagens de domínios externos
+  // },
+  // env: {
+  //   API_URL: 'https://api.example.com', // Variáveis de ambiente acessíveis no lado cliente
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://external-api.example.com/:path*', // Reescreve rotas da API para um endpoint externo
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
