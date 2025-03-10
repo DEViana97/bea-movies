e1a53d3fcf2508fa18e672594167bb310719a15a
deviana97 <mateusviana97@gmail.com>
2025-03-10 19:16:53 -0300

    add teamo

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function MoviesPage() {
   return (
-    <div>Movies</div>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+      <div>
+        <h1>Te amo, vida ❤️!</h1>
+      </div>
+    </div>
   )
 }
