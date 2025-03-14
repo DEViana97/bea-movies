16abb897f7187c95ddaed568a955aef72c412529
Matheus Viana <mateusviana97@gmail.com>
2025-03-14 14:28:28 -0300

    add movie card to movies page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,10 +1,18 @@
+'use client'
 import React from 'react'
+import useFetchMovies from '../../hooks/useFetchMovies'
+import MovieCard from '../../components/movieCard/MovieCard';
+import Loading from '../../components/loading/Loading';
 
 export default function MoviesPage() {
+  const { moviesList, fetchMoviesList, loadingData } = useFetchMovies();
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <div>
-        <h1>Te amo, vida ❤️!</h1>
+        {loadingData && <Loading />}
+        <MovieCard
+          movies={moviesList}
+        />
       </div>
     </div>
   )
