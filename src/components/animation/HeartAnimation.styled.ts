fa4da0665dd3ced6400b178fa1aa6dbe4f843d7e
deviana97 <mateusviana97@gmail.com>
2025-03-14 21:07:03 -0300

    change model heart and animation

--- src/components/animation/HeartAnimation.styled.ts
+++ src/components/animation/HeartAnimation.styled.ts
@@ -3,61 +3,56 @@ import styled, { keyframes } from 'styled-components';
 // Animações
 export const bubbleUp = keyframes`
   0% {
-    opacity: 0;
-    transform: scale(0);
-  }
-  50% {
-    opacity: 1;
-    transform: scale(1);
-  }
-  100% {
-    opacity: 0;
-    transform: scale(1.1);
-  }
+      transform: scale(0.95);
+    }
+    5% {
+      transform: scale(1.1);
+    }
+    39% {
+      transform: scale(0.85);
+    }
+    45% {
+      transform: scale(1);
+    }
+    60% {
+      transform: scale(0.95);
+    }
+    100% {
+      transform: scale(0.9);
+    }
 `;
 
-export const heartFill = keyframes`
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`;
 
 export const BubbleWrapper = styled.div<{ $containerWidth: number; $containerHeight: number }>`
   position: absolute;
-  width: 10vw;
   z-index: 0;
-  height: 10vw;
-  min-width: 70px;
-  min-height: 70px;
   max-width: ${({ $containerWidth }) => Math.min($containerWidth * 0.15, 150)}px;
   max-height: ${({ $containerHeight }) => Math.min($containerHeight * 0.15, 150)}px;
-  color: ${({ theme }) => theme.chihiroBackground.light};
-  border-radius: 50%;
-`;
+  animation: ${bubbleUp} 2s ease-out forwards;
+  
 
-export const BubbleDummy = styled.div`
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  border-radius: inherit;
-  border: .2em solid currentColor;
-  animation: ${bubbleUp} 1200ms ease-out forwards;
-`;
-
-
-export const Heart = styled.svg`
-  width: 100%;
-  height: 100%;
-  background: transparent;
-  path {
-    fill: #f91f0d;
-    stroke: none;
-    animation: ${heartFill} 2s ease-out forwards;
+  .loader {
+    position: relative;
+    width: 40px;
+    height: 60px;
+    animation: heartBeat 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
   }
+
+  .loader:before,
+  .loader:after {
+    content: "";
+    background: red;
+    width: 40px;
+    height: 60px;
+    border-radius: 50px 50px 0 0;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    transform: rotate(45deg);
+    transform-origin: 50% 68%;
+    box-shadow: 5px 4px 5px #0004 inset;
+  }
+  .loader:after {
+    transform: rotate(-45deg);
+  } 
 `;
