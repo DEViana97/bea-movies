a2c914743513d99e98c74f13c33d0c52ecf969c1
deviana97 <mateusviana97@gmail.com>
2025-03-16 01:33:12 -0300

    centralize animation

--- src/components/hamsterLoading/HamsterLoading.styled.ts
+++ src/components/hamsterLoading/HamsterLoading.styled.ts
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+width: 100vw;
+
   .wheel-and-hamster {
     --dur: 1s;
     position: relative;
