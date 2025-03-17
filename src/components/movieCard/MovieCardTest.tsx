a5da21ed8fc36d340293cffe608e2b2e34ebef19
deviana97 <mateusviana97@gmail.com>
2025-03-16 21:56:16 -0300

    change movie card

--- src/components/movieCard/MovieCardTest.tsx
+++ src/components/movieCard/MovieCardTest.tsx
@@ -376,29 +376,21 @@ interface CardProps {
 import React from 'react';
 import styled from 'styled-components';
 
-const Card = ({ movies }: CardProps) => {
+const Card: React.FC<CardProps> = ({ movies }) => {
   return (
     <StyledWrapper>
-      {movies.map((movie) => {
-        return (
-          <StyledCard $imageUrl={movie.image} key={movie.id}>
-            <div className="card__content">
-              <p className="card__title">{movie.name}</p>
-              <p className="card__description">{movie.status ? 'Já assistido' : 'Não assistido'}</p>
-              <StarsContainer>
-                {[...Array(Math.floor(movie.starRating))].map((_, index) => (
-                  <Star key={index} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path d="M316.7 17.8l65.43 132.4l146.4 21.29c26.27 3.796 36.79 36.09 17.75 54.59l-105.9 102.1l25.05 145.5c4.508 26.31-23.23 45.9-46.49 33.7L288 439.6l-130.9 68.7C133.8 520.5 106.1 500.9 110.6 474.6l25.05-145.5L29.72 226.1c-19.03-18.5-8.516-50.79 17.75-54.59l146.4-21.29l65.43-132.4C271.1-6.083 305-5.786 316.7 17.8z" />
-                  </Star>
-                ))}
-              </StarsContainer>
-            </div>
-          </StyledCard>
-        )
-      })}
+      {movies.map((movie) => (
+        <StyledCard $imageUrl={movie.image} key={movie.id}>
+          <div className="card__content">
+            <p className="card__title">{movie.name}</p>
+            <p className="card__title">asaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+            <StarRating initialRating={movie.starRating} />
+          </div>
+        </StyledCard>
+      ))}
     </StyledWrapper>
   );
-}
+};
 
 const StarsContainer = styled.div`
    display: flex;
@@ -415,6 +407,7 @@ const Star = styled.svg`
 
 const StyledWrapper = styled.div`
 display: grid;
+position: relative;
 grid-template-columns: repeat(3, minmax(150px, 1fr));
 gap: 1rem;
 @media (max-width: 768px) {
@@ -443,7 +436,7 @@ const StyledCard = styled.div<{ $imageUrl: string }>`
     box-shadow: 0 8px 16px rgba(255, 255, 255, 0.2);
   }
 
-  & div {
+  .card__content {
     position: absolute;
     top: 0;
     left: 0;
@@ -452,8 +445,6 @@ const StyledCard = styled.div<{ $imageUrl: string }>`
     padding: 20px;
     box-sizing: border-box;
     background-color: rgba(255, 255, 255, 0.8);
-    backdrop-filter: blur(1px);
-    -webkit-backdrop-filter: blur(1px);
     transform: rotateX(-90deg);
     transform-origin: bottom;
     transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
@@ -468,11 +459,9 @@ const StyledCard = styled.div<{ $imageUrl: string }>`
     font-size: 24px;
     color: #333;
     font-weight: 700;
+    margin-top: 10px;
   }
 
-  &:hover svg {
-    scale: 0;
-  }
 
   .card__description {
     margin: 10px 0 0;
