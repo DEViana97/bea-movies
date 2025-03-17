b5a349a7f839e96429e0946f55bfb471308584c1
Matheus Viana <mateusviana97@gmail.com>
2025-03-17 14:39:26 -0300

    add movie list

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -3,16 +3,17 @@ import React from 'react'
 import useFetchMovies from '../../hooks/useFetchMovies'
 import HamsterLoading from '../../components/hamsterLoading/HamsterLoading';
 import Card from '../../components/movieCard/MovieCardTest';
+import MovieList from '../../components/movieList/MovieList';
 
 export default function MoviesPage() {
-  const { moviesList, fetchMoviesList, loadingData } = useFetchMovies();
+  const { moviesList, fetchMoviesList, loadingData, moviesTMDB } = useFetchMovies('movie');
 
   if (loadingData) {
     return <HamsterLoading />
   }
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', }}>
-      <Card movies={moviesList} />
+    <div>
+      <MovieList movies={moviesTMDB} />
     </div>
   )
 }
