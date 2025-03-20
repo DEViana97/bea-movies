f7024c1563502bbf043de277316e7525ee355518
deviana97 <mateusviana97@gmail.com>
2025-03-20 17:22:54 -0300

    add icons

--- src/components/iconManager/index.tsx
+++ src/components/iconManager/index.tsx
@@ -1,4 +1,6 @@
 import WaveBackground from "../icons/WaveBackground";
+import ArrowRightIcon from "../icons/arrowRight";
+import ArrowBackIcon from "../icons/arrowBack";
 
 
 export interface IconProps {
@@ -10,6 +12,8 @@ export interface IconProps {
 
 export enum IconTypes {
   Wave = 'WAVE',
+  ArrowRight = 'ARROW_RIGHT',
+  ArrowBack = 'ARROW_BACK'
 }
 interface IconManagerProps {
   iconType: IconTypes | undefined;
@@ -25,6 +29,10 @@ const IconManager: React.FC<IconManagerProps> = ({ iconType, ...props }) => {
   switch (iconType) {
     case IconTypes.Wave:
       return <WaveBackground width={props.width} height={props.height} foregroundColor={props.foregroundColor} />;
+    case IconTypes.ArrowRight:
+      return <ArrowRightIcon width={props.width} height={props.height} foregroundColor={props.foregroundColor} />;
+    case IconTypes.ArrowBack:
+      return <ArrowBackIcon width={props.width} height={props.height} foregroundColor={props.foregroundColor} />;
 
     default:
       return null;
---
+++ src/components/icons/arrowRight/index.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from "../../iconManager";
+
+
+const ArrowRightIcon: React.FC<IconProps> = (props: IconProps) => (
+  <svg
+    width={props.width}
+    height={props.height}
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    transform="scale(-1, 1)"
+  >
+    <path d="M11.3327 5.33329H3.21935L6.94602 1.60663L5.99935 0.666626L0.666016 5.99996L5.99935 11.3333L6.93935 10.3933L3.21935 6.66663H11.3327V5.33329Z"
+      fill={props.foregroundColor}
+    />
+  </svg>
+);
+
+export default ArrowRightIcon
