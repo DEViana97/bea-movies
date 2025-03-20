c3b468071e7973e1dc0334133b373d9d6c24e6dc
deviana97 <mateusviana97@gmail.com>
2025-03-20 20:47:12 -0300

    remove loading from movies page

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,19 +1,18 @@
 'use client'
+
 import React from 'react'
 import useFetchMovies from '../../hooks/useFetchMovies'
-import HamsterLoading from '../../components/hamsterLoading/HamsterLoading';
-import Card from '../../components/movieCard/MovieCardTest';
 import MovieList from '../../components/movieList/MovieList';
 
 export default function MoviesPage() {
-  const { moviesList, fetchMoviesList, loadingData, moviesTMDB } = useFetchMovies('movie');
-
-  if (loadingData) {
-    return <HamsterLoading />
-  }
+  const {loadingData, moviesTMDB, fetchTMDBMoviesList, metaData } = useFetchMovies('movie');
   return (
-
-    <MovieList movies={moviesTMDB} />
-
+      <MovieList 
+      movies={moviesTMDB} 
+      type="movie" 
+      fetchMoviesList={fetchTMDBMoviesList} 
+      metaData={metaData}
+      loadingData={loadingData} 
+       />
   )
 }
--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { MovieTMDB } from '../../interface/movie'
 import MovieCard from '../movieCard/MovieCard';
+import Pagination, { ItemsPerPage } from '../pagination/Pagination';
+import { Params } from '../../interface/params';
+import HamsterLoading from '../hamsterLoading/HamsterLoading';
+
 
 interface MovieListProps {
   movies: MovieTMDB[];
+  type: string;
+  fetchMoviesList: (type: string, params?: Params) => void;
+  metaData: any;
+  loadingData: boolean;
 }
-function MovieList({movies}: MovieListProps) {
+function MovieList({ movies, type, fetchMoviesList, metaData, loadingData }: MovieListProps) {
+  const [activePaginationPage, setActivePaginationPage] = useState(1);
+  const [searchBoxQuery, setSearchBoxQuery] = useState<string>("");
+  const [itemsPerPage, setItemsPerPage] = useState(ItemsPerPage.twenty);
+
+
+  useEffect(() => {
+    fetchMoviesList(type, {
+      page: activePaginationPage,
+    })
+  }, [activePaginationPage]);
+
+  // if (loadingData) {
+  //   return <HamsterLoading />
+  // }
+
+
+
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100vw'}}>
-      <ul style={{display: 'grid', gridTemplateColumns: 'repeat(auto-fill, 200px)', gap: '16px', listStyle: 'none', width: '90vw', justifyContent: 'end', alignItems: 'center', padding: '20px 0'}}>
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </ul>
-    </div>
+    <Suspense fallback={<HamsterLoading />}>
+      {loadingData ? (
+        <HamsterLoading />) : (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100vw' }}>
+          <ul style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, 200px)', gap: '16px', listStyle: 'none', width: '90vw', justifyContent: 'end', alignItems: 'center', padding: '20px 0' }}>
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </ul>
+          <Pagination
+            currentPage={activePaginationPage}
+            setCurrentPage={setActivePaginationPage}
+            totalItens={metaData.total_results}
+            itemsPerPage={itemsPerPage}
+          />
+        </div>
+      )}
+    </Suspense>
   )
 }
 
