3414ea252e3a856bc8c98219f64d408d0f39362c
deviana97 <mateusviana97@gmail.com>
2025-04-24 18:15:57 -0300

    remove buttons

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -4,7 +4,6 @@ import React, { useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import styled from 'styled-components'
 import Autoplay from 'embla-carousel-autoplay'
-import { ChevronRight, ChevronLeft } from 'lucide-react';
 import { Movie, MovieTMDB } from '../../interface/movie'
 import MovieSlide from '../movieSlide/MovieSlide'
 
@@ -20,36 +19,26 @@ box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
 & .embla__viewport {
   box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
   height: 80vh;
-  width: 95%;
+  width: 100vw;
   overflow: hidden;
 
-  @media (max-width: 820px) {
+  @media (max-width: 320px) {
+    height: 40dvh;
   }
-  /* Small devices such as large phones (640px and up) */
 
-
-/* Medium devices such as tablets (768px and up) */
 @media only screen and (max-width: 768px) {
-  height: 40vh;
+  height: 50dvh;
 }
+
+@media only screen and (max-width: 600px) {
+   height: 30dvh;
+ }
 
 /* Large devices such as laptops (1024px and up) */
 @media only screen and (max-width: 1024px) {
-  height: 50vh;
+  height: 40dvh;
 }
 
-/* Largest devices such as desktops (1280px and up) */
-@media only screen and (min-width: 1280px) {
-  height: 80vh;
-}
-
-
-  /* @media (max-width: 768px) {
-    height: 40vh;
-  }
-  @media (max-width: 500px) {
-    height: 30vh;
-  } */
 }
 
 & .embla__container {
@@ -140,10 +129,6 @@ function EmblaCarousel({ movies, loadingData, moviesWatched }: EmblaCarouselProp
 
   return (
     <CarouselContainer className='embla'>
-      <button>
-        <ChevronLeft onClick={scrollPrev} size={40} color='white' style={{ cursor: 'pointer' }} />
-      </button>
-
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {movies.map((movie) => (
@@ -152,9 +137,6 @@ function EmblaCarousel({ movies, loadingData, moviesWatched }: EmblaCarouselProp
           }
         </div>
       </div>
-      <button>
-        <ChevronRight onClick={scrollNext} size={40} color='white' style={{ cursor: 'pointer' }} />
-      </button>
     </CarouselContainer>
   )
 }
