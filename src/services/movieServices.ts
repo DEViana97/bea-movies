20d24f776bf5b286249d905b4fc63215498f4ed3
deviana97 <mateusviana97@gmail.com>
2025-04-24 18:57:50 -0300

    add params

--- src/services/movieServices.ts
+++ src/services/movieServices.ts
@@ -3,7 +3,13 @@ import { api } from './api';
 
 export const movieService = {
   getMovies: async (params?: Params) => {
-    const url = params ? `/api/movies?${params}` : '/api/movies';
+    const cleanParams = params
+      ? Object.fromEntries(
+          Object.entries(params).filter(([_, value]) => value != null && value !== '')
+        )
+      : {};
+      const queryString = new URLSearchParams(cleanParams).toString();
+    const url = params ? `/api/movies?${queryString}` : '/api/movies';
     return await api.get(url);
   },
   getMoviesTMDB: async (type: string, searchBy: string ,params?: Params) => {
